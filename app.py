856613e27b3149d644a0a9152bcfd0522997c825
Arjun Lakhanpal <144582358+Arjunlakhanpall@users.noreply.github.com>
2025-05-14 16:33:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,66 +1,72 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, request, jsonify
 import random
-import copy
 
 app = Flask(__name__)
 
-def is_valid(board, row, col, num):
-    for i in range(9):
-        if board[row][i] == num or board[i][col] == num:
-            return False
-    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
-    for i in range(3):
-        for j in range(3):
-            if board[start_row + i][start_col + j] == num:
+# Function to generate a new Sudoku puzzle
+def generate_sudoku():
+    # A simple, pre-defined Sudoku puzzle for the sake of example
+    board = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+    return board
+
+# Function to solve the Sudoku puzzle
+def solve_sudoku(board):
+    # Solve the puzzle using a backtracking algorithm
+    def is_valid(board, row, col, num):
+        for i in range(9):
+            if board[row][i] == num or board[i][col] == num:
                 return False
-    return True
+        box_row = row - row % 3
+        box_col = col - col % 3
+        for i in range(3):
+            for j in range(3):
+                if board[i + box_row][j + box_col] == num:
+                    return False
+        return True
+    
+    def solve(board):
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] == 0:
+                    for num in range(1, 10):
+                        if is_valid(board, row, col, num):
+                            board[row][col] = num
+                            if solve(board):
+                                return True
+                            board[row][col] = 0
+                    return False
+        return True
 
-def solve(board):
-    for row in range(9):
-        for col in range(9):
-            if board[row][col] == 0:
-                for num in range(1, 10):
-                    if is_valid(board, row, col, num):
-                        board[row][col] = num
-                        if solve(board):
-                            return True
-                        board[row][col] = 0
-                return False
-    return True
+    solve(board)
+    return board
 
-def generate_board():
-    board = [[0 for _ in range(9)] for _ in range(9)]
-    for _ in range(15):
-        row, col = random.randint(0, 8), random.randint(0, 8)
-        num = random.randint(1, 9)
-        if is_valid(board, row, col, num):
-            board[row][col] = num
-    solved = copy.deepcopy(board)
-    solve(solved)
-    for _ in range(40):
-        r, c = random.randint(0, 8), random.randint(0, 8)
-        solved[r][c] = 0
-    return solved
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
+# Endpoint to get a new Sudoku puzzle
 @app.route('/new', methods=['GET'])
-def new_game():
-    board = generate_board()
-    return jsonify(board=board)
+def new_puzzle():
+    board = generate_sudoku()
+    return jsonify({"board": board})
 
+# Endpoint to solve a Sudoku puzzle
 @app.route('/solve', methods=['POST'])
-def solve_sudoku():
-    data = request.json
-    board = data.get('board')
-    if not board:
-        return jsonify({'error': 'Board is missing'}), 400
-    if solve(board):
-        return jsonify({'solution': board})
-    else:
-        return jsonify({'error': 'No solution exists'}), 400
+def solve_puzzle():
+    data = request.get_json()
+    board = data.get("board")
+    
+    if not board or len(board) != 9 or any(len(row) != 9 for row in board):
+        return jsonify({"error": "Invalid board format. The board should be a 9x9 grid."}), 400
+    
+    solved_board = solve_sudoku(board)
+    return jsonify({"solved_board": solved_board})
 
 if __name__ == '__main__':
     app.run(debug=True)
