047fabc13c2ffc86ca9f0c6b81405dab57971c7e
Arjun Lakhanpal <144582358+Arjunlakhanpall@users.noreply.github.com>
2025-05-15 07:56:19 +0530

    Create sudoku.py

---
+++ sudoku.py
@@ -0,0 +1,37 @@
+# sudoku.py
+
+def is_valid(board, num, pos):
+    row, col = pos
+    # Check row
+    if num in board[row]:
+        return False
+    # Check column
+    if num in [board[i][col] for i in range(9)]:
+        return False
+    # Check 3x3 box
+    box_x, box_y = col // 3, row // 3
+    for i in range(box_y*3, box_y*3+3):
+        for j in range(box_x*3, box_x*3+3):
+            if board[i][j] == num:
+                return False
+    return True
+
+def find_empty(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    row, col = find
+    for num in range(1, 10):
+        if is_valid(board, num, (row, col)):
+            board[row][col] = num
+            if solve(board):
+                return True
+            board[row][col] = 0
+    return False
